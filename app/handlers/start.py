d552795395efb74939cf37a961feaf2ce13d66cc
rustamfaiz <rust_f@icloud.com>
2025-07-03 15:55:14 -0700

    Update start.py

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,1 +1,10 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+router = Router()
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer("✅ Бот запущен. Готов к работе.")
 
