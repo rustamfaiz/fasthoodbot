bc2c90dbcb4a3267bd71ca30c001dac2d2339f55
rustamfaiz <rust_f@icloud.com>
2025-07-03 15:53:44 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from app.config import BOT_TOKEN
+from app.handlers import start
+
+async def main():
+    bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+
+    dp.include_router(start.router)
+
+    print("🤖 Бот запущен.")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
