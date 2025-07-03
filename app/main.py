8cfa4bf129a6332b686b8c67ed8d8172e5822c2e
rustamfaiz <rust_f@icloud.com>
2025-07-03 16:24:21 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
-from app.config import BOT_TOKEN
-from app.handlers import start
+from config import BOT_TOKEN
+from handlers import start
 
 async def main():
     bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
