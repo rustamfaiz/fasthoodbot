f1df5cf215e25ae572fbd18aef6af8a67dbfd59f
rustamfaiz <rust_f@icloud.com>
2025-07-04 03:03:21 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import BOT_TOKEN
-from handlers import start, region, user_data
+from handlers import start, region, user_data, test_generate, book
 
 async def main():
     bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
@@ -13,12 +13,11 @@ async def main():
     dp.include_router(start.router)
     dp.include_router(region.router)
     dp.include_router(user_data.router)
+    dp.include_router(test_generate.router)
+    dp.include_router(book.router)
 
-    print("🤖 Бот запущен.")
+    print("🤖 Бот запущен!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
-from handlers import test_generate
-...
-dp.include_router(test_generate.router)
