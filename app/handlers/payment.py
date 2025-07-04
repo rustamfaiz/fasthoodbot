58cdc657a004b24049aae292d370f032d413bb32
rustamfaiz <rust_f@icloud.com>
2025-07-04 04:18:53 -0700

    Update payment.py

--- app/handlers/payment.py
+++ app/handlers/payment.py
@@ -2,7 +2,7 @@ from aiogram import Router, types, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from app.utils.pdf_generator import generate_personal_pdf
+from utils.pdf_generator import generate_personal_pdf
 
 router = Router()
 
